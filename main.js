2902ddb0b4b26094aa7e23f249560373f25f501b
THEHUSTLER001 <thehustlerultra001@gmail.com>
2024-07-22 18:25:14 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header__content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".header__content .header__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+ScrollReveal().reveal(".header__content .socials", {
+  ...scrollRevealOption,
+  delay: 2500,
+});
+ScrollReveal().reveal(".header__bar", {
+  ...scrollRevealOption,
+  delay: 3000,
+});
+ScrollReveal().reveal(".services", {
+  ...scrollRevealOption,
+  origin: "top",
+  delay: 400,
+});
+
+ScrollReveal().reveal(".product-container", {
+  ...scrollRevealOption,
+  origin: "left",
+  delay: 800,
+});
+
+ScrollReveal().reveal(".form-container", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 1200,
+});
+
+function toggleText() {
+  var textContainer = document.getElementById('textContainer');
+  var button = document.querySelector('.btn');
+  if (textContainer.classList.contains('expanded')) {
+      textContainer.classList.remove('expanded');
+      button.textContent = "Discover More";
+  } else {
+      textContainer.classList.add('expanded');
+      button.textContent = "See Less";
+  }
+}
+
+
+
